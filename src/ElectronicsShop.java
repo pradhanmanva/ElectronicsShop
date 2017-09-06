7fb4565cbb0774e6fa4b4e4c92510ca886c8b175
Manva Pradhan <pradhanmanva@gmail.com>
2017-09-06 00:31:51 -0500

    tested

--- src/ElectronicsShop.java
+++ src/ElectronicsShop.java
@@ -1,12 +1,16 @@
+import com.sun.javafx.binding.StringFormatter;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class ElectronicsShop {
     public static void main(String args[]) {
         //declaring variables
         int n[];
         int m[];
-        int total;
-        int canSpend[][];
+        int s;
         int size_n;
         int size_m;
 
@@ -17,14 +21,44 @@ public class ElectronicsShop {
         String line3 = scanner.nextLine();
 
         //processing
-        String totalspend = "";
-        for (int i = 0; i < line1.length(); i++) {
-            if (line1.charAt(i) == ' ') {
-                break;
+        String line_1[] = line1.split(" ");
+        String line_2[] = line2.split(" ");
+        String line_3[] = line3.split(" ");
+        try {
+            s = Integer.parseInt(line_1[0]); // the value of s
+            size_n = Integer.parseInt(line_1[1]);
+            size_m = Integer.parseInt(line_1[2]);
+            n = new int[size_n];
+            m = new int[size_m];
+
+            if (n.length != line_2.length)
+                exit(400);
+            if (m.length != line_3.length)
+                exit(500);
+
+            for (int i = 0; i < n.length; i++) {
+                n[i] = Integer.parseInt(line_2[i]);
+                //System.out.print(n[i]+ " ");
             }
-            totalspend += line1.charAt(i);
+            for (int i = 0; i < m.length; i++) {
+                m[i] = Integer.parseInt(line_3[i]);
+                //System.out.print(m[i]+" ");
+            }
+
+            //working on the problem
+            int amt = -1;
+            for (int i = 0; i < n.length; i++) {
+                for (int j = 0; j < m.length; j++) {
+                    if (n[i] + m[j] <= s) {
+                        amt = n[i] + m[j];
+                        continue;
+                    }
+                    else break;
+                }
+            }
+            System.out.print(amt);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        total = Integer.parseInt(totalspend);
-        System.out.println(total);
     }
 }
